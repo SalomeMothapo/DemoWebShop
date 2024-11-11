41fe5e50ee76448ad138139d08fe57b1ddfb6e55
priscilla <priscillamuleya@gmail.com>
2024-11-11 16:18:44 +0200

    test

--- src/test/java/runner/TestRunners.java
+++ src/test/java/runner/TestRunners.java
@@ -5,8 +5,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/main/resources/AutomationTestside.feature",
-            glue = {"hooks","pages"},
+        glue = {"hooks", "pages"},
         monochrome = true,
-        publish = true)
-public class TestRunners extends AbstractTestNGCucumberTests{
+        publish = true,
+        plugin = {"json:target/cucumber.json", "html:target/site/cucumber-pretty"})
+public class TestRunners extends AbstractTestNGCucumberTests {
 }
